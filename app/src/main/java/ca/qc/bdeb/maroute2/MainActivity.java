2026e69fa46d1c3541a4b09c1e7a9c4e8ff51302
Jason <sysbender@gmail.com>
2016-06-03 11:09:55 -0400

    ma.route - tabs work and added image buttons

---
+++ app/src/main/java/ca/qc/bdeb/maroute2/MainActivity.java
@@ -0,0 +1,173 @@
+package ca.qc.bdeb.maroute2;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+    private static final int TABNUM = 3;
+    //layout for tab
+    private LinearLayout[] mTabLayout = new LinearLayout[TABNUM];
+    private int[] mTabLayoutID = {R.id.id_tab_1, R.id.id_tab_2, R.id.id_tab_3};
+    //image for tab
+    private ImageButton[] mTabImageButton = new ImageButton[TABNUM];
+    private int[] mTabImageButtonID = {R.id.id_tab_1_img, R.id.id_tab_2_img, R.id.id_tab_3_img};
+    //color, gray picture for table images
+    private int[] mTabImageColorID = {R.mipmap.tab_1_c, R.mipmap.tab_2_c, R.mipmap.tab_3_c};
+    private int[] mTabImageGrayID = {R.mipmap.tab_1_g, R.mipmap.tab_2_g, R.mipmap.tab_3_g};
+
+
+    private Fragment[] mTabFragment = {new Tab1Fragment(), new Tab2Fragment(), new Tab3Fragment()};
+    //private Fragment[] mTabFragment = {null,null, null} ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+        initViews(); //
+        initFragments();
+        initEvents(); //
+        selectTab(0); //
+    }
+
+    private void initFragments() {
+        // mTabFragment = new
+
+    }
+
+
+    //setOnclickListener for tab
+    private void initEvents() {
+        for (int i = 0; i < TABNUM; i++) {
+            mTabLayout[i].setOnClickListener(this);
+        }
+
+    }
+
+    private void initViews() {
+        for (int i = 0; i < TABNUM; i++) {
+            mTabLayout[i] = (LinearLayout) findViewById(mTabLayoutID[i]);
+            mTabImageButton[i] = (ImageButton) findViewById(mTabImageButtonID[i]);
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        setGrayAll();
+        switch (v.getId()) {
+            case R.id.id_tab_1:
+                showTab(0);
+                break;
+            case R.id.id_tab_2:
+                showTab(1);
+                break;
+            case R.id.id_tab_3:
+                showTab(2);
+                break;
+        }
+    }
+
+
+    private void selectTab(int t) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        hideFragments(transaction);
+
+        switch (t) {
+            case 0:
+                mTabImageButton[t].setImageResource(mTabImageColorID[t]);
+
+                if (mTabFragment[t] == null) {
+                    mTabFragment[t] = new Tab1Fragment();
+                    transaction.add(R.id.id_content, mTabFragment[t]);
+                    transaction.show(mTabFragment[t]);
+                } else {
+                    transaction.show(mTabFragment[t]);
+                }
+                Log.e("ma-route", "select " + t);
+                break;
+            case 1:
+                mTabImageButton[t].setImageResource(mTabImageColorID[t]);
+                if (mTabFragment[t] == null) {
+                    mTabFragment[t] = new Tab2Fragment();
+                    transaction.add(R.id.id_content, mTabFragment[t]);
+                    transaction.show(mTabFragment[t]);
+                } else {
+                    transaction.show(mTabFragment[t]);
+                }
+                Log.e("ma-route", "select " + t);
+                break;
+            case 2:
+                mTabImageButton[t].setImageResource(mTabImageColorID[t]);
+                if (mTabFragment[t] == null) {
+                    mTabFragment[t] = new Tab3Fragment();
+                    transaction.add(R.id.id_content, mTabFragment[t]);
+                    transaction.show(mTabFragment[t]);
+                } else {
+                    transaction.show(mTabFragment[t]);
+                }
+                Log.e("ma-route", "select " + t);
+                break;
+        }
+        transaction.commit();
+    }
+
+
+    private void showTab(int t) {
+        //change img
+        mTabImageButton[t].setImageResource(mTabImageColorID[t]);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        //hideFragments(ft);
+
+
+        for (int i = 0; i < TABNUM; i++) {
+            if (i == t) { //show
+                if (!mTabFragment[t].isAdded()) {
+                    ft.add(R.id.id_content, mTabFragment[t]);
+                }
+                ft.show(mTabFragment[t]);
+
+            } else { //hide
+                if (mTabFragment[i].isAdded()) {
+                    ft.hide(mTabFragment[i]);
+                }
+            }
+        }
+        ft.commit();
+    }
+
+
+    private void hideFragments(FragmentTransaction transaction) {
+        for (int i = 0; i < TABNUM; i++) {
+            if (mTabFragment[i] != null) {
+                transaction.hide(mTabFragment[i]);
+                Log.e("ma-route", "hide " + i);
+            }
+        }
+
+    }
+
+    private void setGrayAll() {
+        for (int i = 0; i < TABNUM; i++) {
+            mTabImageButton[i].setImageResource(mTabImageGrayID[i]);
+        }
+    }
+
+}
---
+++ app/src/main/java/ca/qc/bdeb/maroute2/Tab3Fragment.java
@@ -0,0 +1,20 @@
+package ca.qc.bdeb.maroute2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jason on 6/3/2016.
+ */
+public class Tab3Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab3_layout, container, false);
+        return view;
+    }
+}
